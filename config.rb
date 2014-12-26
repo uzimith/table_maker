3a2daf81b91c83454adc2bdbf8ffe22946281780
uzimith <uzimith.x9@gmail.com>
2014-12-26 15:22:23 +0900

    use polymer.

---
+++ config.rb
@@ -0,0 +1,60 @@
+###
+# Compass
+###
+
+# Change Compass configuration
+compass_config do |config|
+  config.output_style = :nest
+  config.line_comments = false
+end
+require 'compass-normalize'
+require 'breakpoint'
+
+###
+# Sprockets
+###
+
+sprockets.append_path 'components'
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+with_layout :layout do
+  page "/*", :layout => "layout"
+end
+
+with_layout :layout do
+  page "/elements/*", :layout => false
+end
+
+###
+# Helpers
+###
+
+helpers do
+  def lang(path)
+    I18n.locale.to_s + "/" + path
+  end
+end
+
+
+activate :automatic_image_sizes
+configure :development do
+  activate :livereload
+end
+
+set :css_dir, 'style'
+set :js_dir, 'js'
+set :images_dir, 'images'
+set :slim, { pretty: true, sort_attrs: false, format: :html }
+Tilt::CoffeeScriptTemplate.default_bare = true
+
+
+configure :build do
+  # activate :minify_css
+  # activate :minify_javascript
+  # activate :asset_hash
+  # activate :relative_assets
+  # set :http_prefix, "/Content/images/"
+end
